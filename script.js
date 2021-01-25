7b4846b7386dd3ffc95a1d89faa650f68010a9bc
drew <andrew_bayola@yahoo.com>
2021-01-25 17:18:19 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Your code here.
+// Printing a triangle with hashtags 
+
+let result = '#';
+let counter = 0;
+
+
+while (counter <= 6) {
+  console.log(result);
+  result= result + '#'
+  counter ++;
+  
+}
